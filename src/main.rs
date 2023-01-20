1fca9f0e54c24218dfda6ece0646a24d79416de7
Daniel.Barton446 <daniel.barton446@gmail.com>
2023-01-19 22:02:54 -0600

    adds unformatted code to test format action

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-#![allow(clippy::style)]
-
 #[test]
 fn one_equals_one() {
-    assert_eq!(1,1)
+    assert_eq!(1, 1)
 }
 
 fn main() {
     println!("Hello, world!");
-    println!("Hello, world AGAIN!");
-    println!("Great seeing you world!");
+        println!("Hello, world AGAIN!");
     println!("HECK");
     let x = --2;
 }
