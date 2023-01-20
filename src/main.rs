d68771cd98308317c45855d41ad58f34a61a6578
Daniel.Barton446 <daniel.barton446@gmail.com>
2023-01-19 20:22:00 -0600

    clippy complaining

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+#![allow(clippy::style)]
 
 #[test]
 fn one_equals_one() {
-assert_eq!(1,1)
+    assert_eq!(1,1)
 }
 
 fn main() {
     println!("Hello, world!");
     println!("Hello, world AGAIN!");
     println!("Great seeing you world!");
+    let x = --2;
 }
