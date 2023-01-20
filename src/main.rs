4976f815452fd33b1899d2b7e39834008d53514b
Daniel.Barton446 <daniel.barton446@gmail.com>
2023-01-19 20:00:17 -0600

    adds failing test

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ fn one_equals_one() {
     assert_eq!(1,1)
 }
 
+#[test]
+fn one_not_one() {
+    assert_ne!(1,1)
+}
+
 fn main() {
     println!("Hello, world!");
     println!("Hello, world AGAIN!");
